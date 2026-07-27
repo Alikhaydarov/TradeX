eb902afddf885a13a4693808f58ef8c8b8f37d46
Ali Khaydarov <141232212+Alikhaydarov@users.noreply.github.com>
2026-07-28 03:17:12 +0900

    Replace dashboard cleanup hack with Tailwind boundary

--- src/components/journal/journal-workspace.tsx
+++ src/components/journal/journal-workspace.tsx
@@ -6,6 +6,13 @@ import { Journal as LegacyJournal } from "../journal";
 
 export type JournalWorkspaceProps = ComponentProps<typeof LegacyJournal>;
 
+const HIDE_DUPLICATE_DASHBOARD_STATS =
+  "contents [&_.animate-page-in>div.space-y-3>div[class*='grid'][class*='grid-cols-2'][class*='xl:grid-cols-5']]:hidden";
+
 export function JournalWorkspace(props: JournalWorkspaceProps) {
-  return <LegacyJournal {...props} />;
+  return (
+    <div className={HIDE_DUPLICATE_DASHBOARD_STATS}>
+      <LegacyJournal {...props} />
+    </div>
+  );
 }
