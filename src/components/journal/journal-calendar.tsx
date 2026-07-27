7f30369a6b476d7f0bc7da05fc5e49ec2ef0a9f7
Ali Khaydarov <141232212+Alikhaydarov@users.noreply.github.com>
2026-07-28 03:53:12 +0900

    refactor: move calendar width contract to Tailwind boundary

--- src/components/journal/journal-calendar.tsx
+++ src/components/journal/journal-calendar.tsx
@@ -12,6 +12,16 @@ const CalendarWorkspaceV2 = dynamic(
   { ssr: false, loading: () => <WorkspaceSectionSkeleton /> },
 );
 
+const CALENDAR_TAILWIND_CONTRACT = [
+  "contents",
+  "[&_.mx-auto.max-w-\[1420px\]]:box-border [&_.mx-auto.max-w-\[1420px\]]:w-full [&_.mx-auto.max-w-\[1420px\]]:max-w-full [&_.mx-auto.max-w-\[1420px\]]:mx-0",
+  "lg:[&_.mx-auto.max-w-\[1420px\]]:!mx-auto lg:[&_.mx-auto.max-w-\[1420px\]]:!w-[min(1320px,calc(100%-11rem))] lg:[&_.mx-auto.max-w-\[1420px\]]:!max-w-[1320px] lg:[&_.mx-auto.max-w-\[1420px\]]:!px-0",
+].join(" ");
+
 export function JournalCalendar() {
-  return <CalendarWorkspaceV2 />;
+  return (
+    <div className={CALENDAR_TAILWIND_CONTRACT}>
+      <CalendarWorkspaceV2 />
+    </div>
+  );
 }
