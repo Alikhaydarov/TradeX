1a1ee21b7e333f3ffa8b16952c58796da1f68baf
Ali Khaydarov <141232212+Alikhaydarov@users.noreply.github.com>
2026-07-28 03:52:59 +0900

    refactor: move journal width contract to Tailwind boundary

--- src/components/journal/journal-workspace.tsx
+++ src/components/journal/journal-workspace.tsx
@@ -6,12 +6,16 @@ import { Journal as LegacyJournal } from "../journal";
 
 export type JournalWorkspaceProps = ComponentProps<typeof LegacyJournal>;
 
-const HIDE_DUPLICATE_DASHBOARD_STATS =
-  "contents [&_.animate-page-in>div.space-y-3>div[class*='grid'][class*='grid-cols-2'][class*='xl:grid-cols-5']]:hidden";
+const JOURNAL_TAILWIND_CONTRACT = [
+  "contents",
+  "[&_.animate-page-in.mx-auto]:box-border [&_.animate-page-in.mx-auto]:w-full [&_.animate-page-in.mx-auto]:max-w-full [&_.animate-page-in.mx-auto]:mx-0",
+  "lg:[&_.animate-page-in.mx-auto]:!mx-auto lg:[&_.animate-page-in.mx-auto]:!w-[min(1320px,calc(100%-11rem))] lg:[&_.animate-page-in.mx-auto]:!max-w-[1320px] lg:[&_.animate-page-in.mx-auto]:!px-0",
+  "[&_.animate-page-in>div.space-y-3>div[class*='grid'][class*='grid-cols-2'][class*='xl:grid-cols-5']]:hidden",
+].join(" ");
 
 export function JournalWorkspace(props: JournalWorkspaceProps) {
   return (
-    <div className={HIDE_DUPLICATE_DASHBOARD_STATS}>
+    <div className={JOURNAL_TAILWIND_CONTRACT}>
       <LegacyJournal {...props} />
     </div>
   );
